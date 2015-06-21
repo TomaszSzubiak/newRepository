2ddc032e3c078889d9216adec49ece206d3d8220
Tomek <Tomek@Tomek-Komputer>
2015-06-21 21:55:03 +0200

    Dodanie stałych pozwalających na łatwy dostęp do danych RMI_ID oraz RMI_PORT

--- RMIInterface/src/com/rmiinterface/Constants.java
+++ RMIInterface/src/com/rmiinterface/Constants.java
@@ -3,7 +3,7 @@ package com.rmiinterface;
 /**Zawiera info o porcie i id polaczenia
  * */
 public class Constants {
-
-		public static final String RMI_ID = "TestRMI";
-		public static final int RMI_PORT = 222;
+	public static final String RMI_ID = "TestRMI";
+	public static final int RMI_PORT = 222;
+	
 }
